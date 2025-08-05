36d3371ea6f1bc0e10dd486a2fedf2f12f5c1b33
hussein-gif <107138142+hussein-gif@users.noreply.github.com>
2025-08-05 19:22:12 +0200

    Updated IndustriesSection.tsx

--- src/pages/PartnerWithUsPage/IndustriesSection.tsx
+++ src/pages/PartnerWithUsPage/IndustriesSection.tsx
@@ -113,7 +113,7 @@ const IndustriesSection: React.FC<IndustriesSectionProps> = ({ isVisible }) => {
         .icon-bg {
           width: 4rem;
           height: 4rem;
-          background: #72ab32;
+          background: #90d541;
           border-radius: 0.5rem;
           display: flex;
           align-items: center;
