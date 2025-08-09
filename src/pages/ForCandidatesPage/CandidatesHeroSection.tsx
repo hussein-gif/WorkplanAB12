f73624923010027f59628b201903f968c43fa26a
hussein-gif <107138142+hussein-gif@users.noreply.github.com>
2025-08-09 19:01:44 +0200

    Change Candidates hero section to a fourth different background image

--- src/pages/ForCandidatesPage/CandidatesHeroSection.tsx
+++ src/pages/ForCandidatesPage/CandidatesHeroSection.tsx
@@ -14,7 +14,7 @@ const CandidatesHeroSection: React.FC<CandidatesHeroSectionProps> = ({
       {/* Background Image */}
       <div className="absolute inset-0">
         <img
-          src="https://images.pexels.com/photos/3184360/pexels-photo-3184360.jpeg"
+          src="https://images.pexels.com/photos/3182773/pexels-photo-3182773.jpeg"
           alt="Professional team meeting and collaboration"
           className="w-full h-full object-cover"
         />
