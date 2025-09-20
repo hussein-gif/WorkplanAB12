46cef54bb21c59d827a63ab17b9e58d46b0965ef
hussein-gif <107138142+hussein-gif@users.noreply.github.com>
2025-09-20 17:28:30 +0200

    Updated AdminLogin.tsx

--- src/admin/AdminLogin.tsx
+++ src/admin/AdminLogin.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { supabase } from '../supabaseClient';
+import { adminSupabase as supabase } from '../../supabaseClient';
+
 import { useNavigate } from 'react-router-dom';
 
 export default function AdminLogin() {
