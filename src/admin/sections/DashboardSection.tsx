053b614a0cb29e8a8740857c8a92733936a770b8
hussein-gif <107138142+hussein-gif@users.noreply.github.com>
2025-09-20 17:27:53 +0200

    Updated DashboardSection.tsx

--- src/admin/sections/DashboardSection.tsx
+++ src/admin/sections/DashboardSection.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { supabase } from '../../supabaseClient';
+import { adminSupabase as supabase } from '../../supabaseClient';
+
 import {
   Application,
   ContactMessage,
